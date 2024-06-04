894f06d3f9f8fa2b6543f5f951ae4473e5525c18
niti-di1977 <60072970+niti-di1977@users.noreply.github.com>
2024-06-04 11:46:26 +0530

    Add files via upload

---
+++ spring-boot-consul/src/test/java/com/example/consul/api/SpringBootConsulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consul.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConsulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
